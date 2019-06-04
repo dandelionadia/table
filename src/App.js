1ac81cfbcd9731f8e057850e6fa86bcc4e9e0d97
dandelionadia <nadia.ridko@gmail.com>
2019-06-04 14:59:01 +0200

    adds real data

--- src/App.js
+++ src/App.js
@@ -1,80 +1,14 @@
 import React from 'react';
 import { Table } from './Table.js'
 import './style.scss';
-
+import DataJson from './data.json';
 
 class App extends React.Component {
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      data: [
-        {
-          data: {
-            "Identification number": "34",
-            "Name": "Joqmo",
-            "Gender": "female",
-            "Risk": "BITES",
-            "Hair length": "6.2000000000",
-            "IQ": "98",
-            "Admission date": "Mon Dec 13 00:00:00 CET 1993",
-            "Last breakdown": "Wed Dec 24 07:14:50 CET 2014",
-            "Yearly fee": "67035",
-            "Knows the Joker?": "true",
-          },
-          kids: {
-            name: {
-              "records": [
-                {
-                  "data": {
-                    "Relative ID": "1007",
-                    "Patient ID": "34",
-                    "Is alive?": "true",
-                    "Frequency of visits": "29"
-                  },
-                  kids: {
-                    has_fone: {
-                      records: [
-                        {
-                          data: {
-                            "Relative ID": "1007",
-                            "Patient ID": "34",
-                            "Is alive?": "true",
-                            "Frequency of visits": "29"
-                          },
-                          kids: {}
-                        }
-                      ]
-                    }
-                  }
-                }
-              ]
-            }
-          },
-        },
-        {
-          data: {
-            "Identification number": "34",
-            "Name": "Joqmo",
-            "Gender": "female",
-            "Risk": "BITES",
-            "Hair length": "6.2000000000",
-            "IQ": "98",
-            "Admission date": "Mon Dec 13 00:00:00 CET 1993",
-            "Last breakdown": "Wed Dec 24 07:14:50 CET 2014",
-            "Yearly fee": "67035",
-            "Knows the Joker?": "true",
-          }
-        }
-      ]
-    }
-  }
-
   render() {
-    const { data } = this.state
     return (
       <div className="App" >
-        <Table data={data} />
+        <Table data={DataJson} />
       </div>
     );
   }
